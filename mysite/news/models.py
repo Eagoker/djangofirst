e3639277f1963bfd1c20e83ece83b28f38d2b0bb
Anton <eagoker@mail.ru>
2024-07-08 15:36:49 +0300

    admin

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,8 +6,13 @@ class News(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ["-created_at"]
