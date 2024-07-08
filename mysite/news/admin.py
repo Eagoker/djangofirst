e23dc025ea00a62e101eddb78215a4e469da810b
Anton <eagoker@mail.ru>
2024-07-08 20:35:52 +0300

    adminlast

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -3,7 +3,7 @@ from .models import News
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'created_at')
+    list_display = ('id', 'title', 'content')
     list_display_links = ('title',)
 
 
