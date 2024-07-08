00a37810a945daba48c53075225cc8afbeebfe55
Anton <eagoker@mail.ru>
2024-07-08 14:22:34 +0300

    templates2

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    news = News.objects.order_by('-created_at')
+    context = {
+        "title": "news list",
+        "news": news,
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 
-def test(request):
-    return HttpResponse('Test world!')
