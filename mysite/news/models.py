9614ebae4ccd650cfde8a7767b4fba9d32a14550
Anton <eagoker@mail.ru>
2024-07-09 22:23:18 +0300

    last commit

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -6,8 +6,17 @@ class News(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20, db_index=True)
+
+    def __str__(self):
+        return self.title
+
