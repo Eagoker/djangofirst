94ae72e8a4f895018cb3e3b6562013ba85e71fbf
Anton <eagoker@mail.ru>
2024-07-11 11:47:38 +0300

    categories

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'News list'})
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'News list',
+        'categories': categories,
+    }
+    return render(request, 'news/index.html', context)
 
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id) 
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories,
+    'category': category})
