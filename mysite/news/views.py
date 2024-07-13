634e4ffbf3ad0f4324c27f3448d2d9a4eb63932b
Anton <eagoker@mail.ru>
2024-07-13 13:22:14 +0300

    first form

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import News, Category
+from .forms import NewsForm
 
 
 def index(request):
@@ -14,8 +15,28 @@ def index(request):
 
 
 def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id) 
-    categories = Category.objects.all()
+    news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
-    return render(request, template_name='news/category.html', context={'news': news, 'categories': categories,
-    'category': category})
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'category': category,
+        'categories': categories,
+    }
+    return render(request, template_name='news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    news_item = News.objects.get(pk=news_id)
+    context = {
+        'news_item': news_item
+    }
+    return render(request, template_name='news/view_news.html', context=context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = NewsForm()
+    return render(request=request, template_name='news/add_news.html', context={'form': form})
