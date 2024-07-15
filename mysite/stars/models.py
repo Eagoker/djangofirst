1f95344fd2f69d77b0b39cfd6a24a1d7c96fb460
Anton <eagoker@mail.ru>
2024-07-15 14:39:49 +0300

    beforeORM

--- mysite/stars/models.py
+++ mysite/stars/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Stars(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=30)
